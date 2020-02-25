38d43aeb6adf0777530a55059863f3c408d42ae3
Samantha Meason <sbmeason@gmail.com>
2020-02-25 23:48:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,15 @@ def kind
     elsif
     (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
+    else 
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+      :equilateral
+    elsif 
+      (@side_1 == @side_2) || (@side_1 == @side_3) || (@side_2 == @side_3)
+      :isosceles
+    elsif 
+      (@side_1 != @side_2) || (@side_1 != @side_3) || (@side2_ != @side_3)
+      :scalene
     end
   end 
 end
