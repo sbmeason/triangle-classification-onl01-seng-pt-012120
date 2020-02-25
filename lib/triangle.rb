f563d2c5ab37b5b9be1018ea4912727693585501
Samantha Meason <sbmeason@gmail.com>
2020-02-25 23:47:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle 
   
   class TriangleError < StandardError
-end
+  end
 
 def initialize(side_1, side_2, side_3)
   @side_1 = side_1
@@ -25,4 +25,5 @@ def kind
       raise TriangleError
     end
   end 
+end
 end
