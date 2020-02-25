72e1a1b4ff79a4360770cf0c8e802a48071785d3
Samantha Meason <sbmeason@gmail.com>
2020-02-25 22:52:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,9 @@ def kind
     if (@side_1 == @side_2) && (@side_2 == @side_3)
       :equilateral
     else
-      (@side_1 == @side_2) || (@side_1 == @side_3)
+      (@side_1 == @side_2) || (@side_1 == @side_3) || (@side2_ == @side_3)
+      :isosceles
+      
       
 end
 end
