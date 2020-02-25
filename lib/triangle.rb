ea9a8926bd00bcc0724289d25221afcf991a200a
Samantha Meason <sbmeason@gmail.com>
2020-02-25 23:48:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,14 +10,6 @@ def initialize(side_1, side_2, side_3)
 end
 
 def kind
-    if (@side_1 == @side_2) && (@side_2 == @side_3)
-      :equilateral
-    elsif 
-      (@side_1 == @side_2) || (@side_1 == @side_3) || (@side_2 == @side_3)
-      :isosceles
-    elsif 
-      (@side_1 != @side_2) || (@side_1 != @side_3) || (@side2_ != @side_3)
-      :scalene
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     elsif
