61376a304ffcf99358052233a7d520515874bb18
Samantha Meason <sbmeason@gmail.com>
2020-02-25 23:45:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ def kind
     elsif 
       (@side_1 != @side_2) || (@side_1 != @side_3) || (@side2_ != @side_3)
       :scalene
-    if (@side_1 <=0) || (@side_2 <=0) || (@side_3 <=0)
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     else 
       
