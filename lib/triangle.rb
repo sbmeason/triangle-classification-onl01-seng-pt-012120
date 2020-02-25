462fe96b8a4dcc8129ae9047e81c8d8e2ebad4f8
Samantha Meason <sbmeason@gmail.com>
2020-02-25 22:35:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,12 @@
 class Triangle 
-attr_accessor :equilateral, :isosceles, :scalene
 
-def initialize()
+def initialize(side_1, side_2, side_3)
+  @side_1 = side_1
+  @side_2 = side_2
+  @side_3 = side_3
 end
 
 def kind
+  
 end
 end
